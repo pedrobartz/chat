1d9c45e5ef6a578d2cebb6a7cd5a34d5cf66e1e9
pedro <bartz.pedro12@gmail.com>
2025-06-25 17:49:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
-# app.py
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-
-app.config['teste'] = 
+app.config['SECRET_KEY'] = 'chat_privado' 
 socketio = SocketIO(app)
 
 
 @app.route('/')
 def index():
-
     return render_template('index.html')
 
 
 @socketio.on('message_from_client')
 def handle_message(data):
     print(f"Mensagem recebida: {data['msg']}")
-
     emit('message_to_clients', data, broadcast=True)
 
 if __name__ == '__main__':
-
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
